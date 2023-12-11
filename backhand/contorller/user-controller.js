f36ad9993d65cad4abbf1592950117d7ec362da4
Faysal9991 <faysaltanvir9991@gmail.com>
2023-12-11 22:23:31 +0600

    user sign up complete

--- backhand/contorller/user-controller.js
+++ backhand/contorller/user-controller.js
@@ -2,6 +2,7 @@
 
 const  User = require("../model/user_model");
 const { check, validationResult } = require('express-validator');
+const mongoose = require("mongoose") ;
 
 
   const getAllUsers = async (req,res, next)=>{
@@ -17,24 +18,33 @@ if(!users){
     return res.status(200).json({users});
 }
 }
-const validate = (req, res, next) => {
+const validate = async (req, res, next) => {
     const validationRules = [
       check('name').isLength({ min: 3 }).withMessage('Name must be at least 3 characters'),
       check('email').isEmail().withMessage('Invalid email address'),
       check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
     ];
-      validationRules.forEach(rule => rule(req, res, next));
+  
+    try {
+    
+      for (const rule of validationRules) {
+        await rule.run(req);
+      }
+  
       const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-       res.status(400).json({ errors: errors.array() });
-
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    } catch (err) {
+      // Handle any errors that might occur during validation
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
     }
   };
+  
 const signUp = async (req,res)=>{
-
     const {name ,email,password} = req.body;
-   
-
     let existingUser;
     try{
         existingUser = await User.findOne({email});
@@ -50,11 +60,29 @@ const signUp = async (req,res)=>{
             password
         });
         try{
-         await  user.save;
-        }catch(err){console.log(err);}
-        return res.status(201).json({user});
+         user.save();
+         res.status(201).json({user});
+        }catch(err){
+          console.log(err);
+        }
+       
     }
 }
+
+ const login = async (req ,res)=>{
+  let existingUser;
+  try{
+    existingUser = await User.findOne({email});
+}catch(err){
+    console.log(err);
+}
+if(!existingUser){
+    return res.status(404).json({message:"User not found please signUp"})
+}else{
+  existingUser = await User.comp({email});
+}
+
+ }
 module.exports = {getAllUsers,validate, signUp};
 
 
